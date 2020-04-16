87e333ef7ab073f5b4b9f89a248d716221a9561d
luckrain7 <763839938@qq.com>
2020-04-16 18:46:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -9,28 +9,22 @@
 set -e
 
 # 生成静态文件
-yarn build
-
-git add -A
-git commit -m 'update'
-git push -f git@e.coding.net:luckrain7/blog.git master:dev
+hexo clean
+hexo generate
 
 # 进入生成的文件夹
 cd public
 
 # 如果是发布到自定义域名
-# echo 'hexo.rain7.top' > CNAME
+echo 'hexo.rain7.top' > CNAME
 
+# git
 git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:luckrain7/luckrain7.github.io.git master
+# 推送
+git push -f git@github.com:LuckRain7/blog.git master:pages
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@e.coding.net:luckrain7/blog.git master
-# 发布到码云 git@gitee.com:LuckRain/MyBlog.git
-# git push  -f git@gitee.com:LuckRain/MyBlog.git master:pages
 
 cd -
